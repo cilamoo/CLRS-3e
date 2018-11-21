77de3715c0df69606f7a0626f4d37e4c1cfb387f
cilamoo <sgydd001@gmail.com>
2018-11-22 00:03:37 +0800

    fix bugs

--- IV Advanced Design and Analysis Techniques/16 Greedy Algorithms/recursive_activity_selector.c
+++ IV Advanced Design and Analysis Techniques/16 Greedy Algorithms/recursive_activity_selector.c
@@ -27,27 +27,26 @@
  * 6 else return Ø
  * 
  * RECURSIVE-ACTIVITY-SELECTOR(s,f,0,n) is Θ(n)
- * /
+*/
+
+#include <stdio.h>
  
- int* recursive_activity_selector(int *, int *, int , int);
+ int recursive_activity_selector(int *, int *, int , int);
  void print_result_set(int *,int *, int *);
+ int set[11] = {0,0,0,0,0,0,0,0,0,0,0};
 
  int main(){
     int i;
     int s[11] = {1, 3, 0, 5, 3, 5, 6, 8, 8, 2, 12};
     int f[12] = {0, 4, 5, 6, 7, 9, 9, 10, 11, 12, 14, 16};
-    int *pset;
-    pset = recursive_activity_selector(s, f, 0, 11);
-    print_result_set(pset, s, f);
+    recursive_activity_selector(s, f, 0, 11);
+    print_result_set(set, s, f);
     return 0;
  }
 
- int* recursive_activity_selector(int *ps, int *pf, int k, int n){
-    int i,m;
-    static int set[11];
+ int recursive_activity_selector(int *s, int *f, int k, int n){
+    int m;
     static int index = 0;
-    for(i = 0; i < 11; i++)
-        set[i] = 0;
     
     m = k + 1;
     while(m <= n && s[m-1] < f[k])
@@ -55,21 +54,20 @@
     
     if(m <= n){
         set[index++] = m;
-        return recursive_activity_selector(ps, pf, m, n);
+        return recursive_activity_selector(s, f, m, n);
     }
     else{
         return 0;
     }
-    
-    return &set;
  }
 
- void print_result_set(int *pset, int *ps; int *pf){
+ void print_result_set(int *pset, int *ps, int *pf){
      int i;
-     printf("activity set is:\n");
-     for(i = 0; i <= 11; i++){
+     printf("Activity set is:\n");
+     for(i = 0; i < 11; i++){
         if(pset[i] !=0 ){
-            printf("A%d start %d finish %d\n",pset[i],ps[m-1],pf[m]);
+           int idx = pset[i];
+           printf("a%d start:%d, finish:%d\n",idx, ps[idx-1], pf[idx]);
         }
      }
  }
