e8499b5e7fe89c1cf63bf6ef2b9a5be8df724c72
cilamoo <sgydd001@gmail.com>
2018-12-17 14:07:00 +0800

    fix bug

--- VI Graph Algorithms/23 Minimum Spanning Trees/MST_Kruskal.c
+++ VI Graph Algorithms/23 Minimum Spanning Trees/MST_Kruskal.c
@@ -44,14 +44,9 @@ typedef struct vertex{
 
 typedef struct edge{
     int s;
-    int d;
+    int t;
     int weight;
 }edge;
-
-void make_set(vertex *x){
-    x->p = x;
-    x->rank = 0;
-}
 
 typedef struct Graph{
     vertex *V;
@@ -60,11 +55,18 @@ typedef struct Graph{
     unsigned int edgenum;
 }Graph;
 
+/*Disjoint-set forests,using heuristics—“union by rank” and “path compression”—can achieve an asymptotically
+optimal disjoint-set data structure.*/
+void make_set(vertex *x){
+    x->p = x;
+    x->rank = 0;
+}
 
-/* UNION operation*/
-void union_opt(vertex *x, vertex *y)
-{
-    link(find_set(x), find_set(y));
+
+vertex* find_set(vertex *x){
+    if(x != x->p)
+        x->p = find_set(x->p);
+    return x->p;
 }
 
 void link(vertex *xp, vertex *yp){
@@ -79,12 +81,11 @@ void link(vertex *xp, vertex *yp){
     }
 }
 
-vertex* find_set(vertex *x){
-    if(x != x->p)
-        x->p = find_set(x->p);
-    return x->p;
+/* UNION operation*/
+void union_opt(vertex *x, vertex *y)
+{
+    link(find_set(x), find_set(y));
 }
-
 
 Graph* graph_alloc(size_t vernum, size_t edgenum){
     Graph *G;
@@ -98,7 +99,7 @@ Graph* graph_alloc(size_t vernum, size_t edgenum){
         return NULL;
     
     G->vernum = (unsigned int)vernum;
-    G->edge =  (unsigned int )edgenum;
+    G->edgenum =  (unsigned int )edgenum;
 
     return G;
 }
@@ -108,7 +109,7 @@ void vertex_init(Graph *G){
     vertex *V = G->V;
     for(i = 0; i < G->vernum; i++){
         V[i].ver = i;
-        make_set(&G[i]);
+        make_set(&V[i]);
     }
 }
 
@@ -123,55 +124,10 @@ void edge_init(Graph *G, int *e){
     }
 }
 
-void graph_release(*G){
+void graph_release(Graph *G){
     free(G->E);
     free(G->V);
     free(G);
-}
-
-
-edge* MST_Kruskal(Graph *G){
-    unsigned int i,n;
-    int s,t;
-    n = G->edgenum;
-    edge **SetA, **ptmp;
-    edge *sorted_edges[n];
-    vertex *V;
-    edge *E;
-
-    if((SetA = (edge**)malloc((size_t)n*(sizeof edge*)))) == NULL){
-        return NULL;
-    }
-
-    ptmp = SetA;
-    
-    for(i = 0; i < n; i++){
-        sorted_edges[i] = &G->E[i];
-        SetA[i] = NULL;
-    }
-
-    quicksort(&sorted_edges[0],0,n-1);
-
-    E = G->E;
-    V = G->V;
-   for(i = 0; i < n; i++){
-        s = E[i].s;
-        t = E[i].t;
-       if(find_set(&V[s]) != find_set(&V[t])){
-           SetA++ = &E[i];
-           union_opt(&V[s], &V[t]);
-       }
-   }
-   
-   return ptemp; 
-}
-
-void print_MST_Kruskal(edge** A){
-    int i = 1;
-    printf("MST: ")
-    while(A++ != NULL){
-        printf("The ith edge is %d -> %d\n", i++, (**A).s,(**A).t);
-    }
 }
 
 void swap(edge **v1, edge **v2){
@@ -204,19 +160,78 @@ void quicksort(edge **A, int p, int r){
     }
 }
 
+void print_quicksort(edge**r, int n){
+    int i;
+    printf("Sorted edges:\n");
+    for(i = 0; i < n; i++){
+        printf("       edge is %d -> %d\n",r[i]->s, r[i]->t);  
+    }
+    printf("\n");
+}
+
+edge** MST_Kruskal(Graph *G){
+    unsigned int i,n;
+    int s,t;
+    n = G->edgenum;
+    edge **SetA, **ptemp;
+    edge *sorted_edges[n];
+    vertex *V;
+    edge *E, *se;
+    
+    if((SetA = (edge**)malloc((size_t)n*sizeof(edge*))) == NULL){
+        return NULL;
+    }
+
+    ptemp = SetA;
+    
+    for(i = 0; i < n; i++){
+        sorted_edges[i] = &G->E[i];
+        SetA[i] = NULL;
+    }
+
+    quicksort(&sorted_edges[0],0,n-1);
+    print_quicksort(&sorted_edges[0],n-1); 
+
+    E = G->E;
+    V = G->V;
+   for(i = 0; i < n; i++){
+        se = sorted_edges[i];
+        s = se->s;
+        t = se->t;
+       if(find_set(&V[s]) != find_set(&V[t])){
+           *SetA++ = se;
+           union_opt(&V[s], &V[t]);
+           printf("discovery edge %d -> %d\n",s,t);
+       }
+   }
+   
+   return ptemp; 
+}
+
+void print_MST_Kruskal(edge** A){
+    int i = 1;
+    printf("\nMST: \n");
+    while( *A != NULL){
+        printf("The %dth edge is %d -> %d\n", i++, (**A).s,(**A).t);
+        A++;
+    }
+}
+
 /*Input from Figure 23.4 on page 632 
 char vertexes[9] = {'a','b','c','d','e','f','g','h','i'};
 int vermap[9] = {0,1,2,3,4,5,6,7,8};
-int edges_weight[42] = {0,1,4, 0,7,8, 1,2,8, 1,7,11, 2,3,7, 2,5,4, 2,8,2, 3,4,9, 4,5,10, 3,5,14, 5,6,2, 6,8,6, 6,7,1, 7,8,1 };
+int edges_weight[42] = {0,1,4, 0,7,8, 1,2,8, 1,7,11, 2,3,7, \
+                        2,5,4, 2,8,2, 3,4,9, 4,5,10, 3,5,14, 5,6,2, 6,8,6, 6,7,1, 7,8,7 };
 */
 int main(){
     char vertexes[9] = {'a','b','c','d','e','f','g','h','i'};
     int vermap[9] = {0,1,2,3,4,5,6,7,8};
-    int edges_weight[42] = {0,1,4, 0,7,8, 1,2,8, 1,7,11, 2,3,7, 2,5,4, 2,8,2, 3,4,9, 4,5,10, 3,5,14, 5,6,2, 6,8,6, 6,7,1, 7,8,1 };
+    int edges_weight[42] = {0,1,4, 0,7,8, 1,2,8, 1,7,11, 2,3,7, \
+                            2,5,4, 2,8,2, 3,4,9, 4,5,10, 3,5,14, 5,6,2, 6,8,6, 6,7,1, 7,8,7 };
     Graph *G;
     edge **SetA, **ptmp;
 
-    if((G = graph_alloc(9,14) == NULL){
+    if((G = graph_alloc(9,14)) == NULL){
         printf("fail to allocate memory \n");
         return -1;
     }
