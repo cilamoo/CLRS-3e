b177d3c2a292a7c722cac7bc771ccaaf40025bf5
cilamoo <sgydd001@gmail.com>
2018-12-25 16:09:21 +0800

    fix bug

--- VI Graph Algorithms/25 All-Pairs Shortest Paths/Floyd_Warshall.c
+++ VI Graph Algorithms/25 All-Pairs Shortest Paths/Floyd_Warshall.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-static int dim;      /*matrix dimension*/
+static int N;      /*matrix dimension*/
 #define INFT 9999
 #define  NIL  -128
 
@@ -12,13 +12,17 @@ int min_weight(int x, int y){
     return (x < y)? x : y;
 }
 
-void result* Floyd_Warshall(int *W,int *P, struct result *r){
+struct result{
+        int *sp;    /*pointer to shortest path matrix */
+        int *pp;     /* pointer to predecessor matrix */
+};
+
+void Floyd_Warshall(int *W,int *P, struct result *pr){
     int i,j,k,x,y;
-    struct result *r;
     int *next_matrix, *next_path;
     int *prev_matrix = W;      /* have less intermediate vertex, that is , k is small */
     int *prev_path = P;
-
+    struct result *r = pr;
     for(k = 0; k < N; k++){
         next_matrix = prev_matrix + N*N;
         next_path = prev_path + N*N;
@@ -26,8 +30,8 @@ void result* Floyd_Warshall(int *W,int *P, struct result *r){
         for(i = 0; i < N; i++){
             for(j = 0; j < N; j++){
                 x = prev_matrix[i*N+j];
-                y = prev_matrix[i*N+k] + prev_matrix[k*N+j]
-                next_matrix[i*N+j] = min(x, y);
+                y = prev_matrix[i*N+k] + prev_matrix[k*N+j];
+                next_matrix[i*N+j] = min_weight(x, y);
                 
                 if( x <= y){
                     next_path[i*N+j] = prev_path[i*N+j];
@@ -41,26 +45,22 @@ void result* Floyd_Warshall(int *W,int *P, struct result *r){
         prev_path = next_path;
     }
     r->sp = next_matrix;
-    r->pp = prev_matrix;
+    r->pp = prev_path;
 }
 
-struct result{
-        int *sp;    /*pointer to shortest path matrix */
-        int *pp;     /* pointer to predecessor matrix */
-};
 
-void print_result(struct reulst *pr){
+void print_result(struct result *pr){
     int i,j;
     int *sp;
     int *pp;
 
     sp = pr->sp;
-    pp = pr->sp;
+    pp = pr->pp;
 
     printf("Matrix Output:\n");
     for(i = 0; i < N; i++){
         for(j=0; j < N;j++){
-         printf("%2d ",sp[i*N+j]);
+         printf("%3d ",sp[i*N+j]);
     }
      printf("\n");
     }
@@ -68,18 +68,18 @@ void print_result(struct reulst *pr){
      printf("Path Matrx Output:\n");
     for(i = 0; i < N; i++){
         for(j=0; j < N;j++){
-         printf("%2d ",pp[i*N+j]);
+         if(pp[i*N+j] == NIL)
+            printf("%3s ","NIL");
+         else
+            printf("%3d ",pp[i*N+j]);
     }
      printf("\n");
     }
 }
 
 int main(){
-    int *W,*r,*P;
+    int *W,*P;
     struct result r;
-    
-    r->sp = NULL;
-    r->pp = NULL;
 
     int input[25] = {0,3,8,INFT,-4, INFT,0,INFT,1,7, INFT,4,0,INFT,INFT,\
              2,INFT,-5,0,INFT, INFT,INFT,INFT,6,0};               /* Graph, Input Matrix*/
@@ -97,17 +97,17 @@ int main(){
     if((P = malloc((size_t)size)) == NULL)    /* predecessor matrix */
     {
         printf("P's memory alloc failed\n");
-        return NULL;
+        exit(1);
     }
 
-    memcpy(W,input,(size_t)N*N*sizeof(int));
-    memcpy(P,parent,(size_t)N*N*sizeof(int));
+    memcpy(W,input,sizeof(input));
+    memcpy(P,parent,sizeof(parent));
 
     Floyd_Warshall(W,P,&r);
     print_result(&r);
 
-    free(W);
-    free(P);
+   free(W);
+   free(P);
 
     return 0;
 }
--- VI Graph Algorithms/25 All-Pairs Shortest Paths/transitive_closure.c
+++ VI Graph Algorithms/25 All-Pairs Shortest Paths/transitive_closure.c
@@ -3,6 +3,7 @@ TODO：implement table search to determine  whether an edge belongs to Graph G*/
 
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 static int N;
 
@@ -28,7 +29,7 @@ int* transitive_closure(int *T){
     return T_next;
 }
 
-print_result(int *r){
+void print_result(int *r){
     int i,j;
     printf("Output Transitive Closure\n");
     for(i = 0; i < N; i++){
@@ -40,21 +41,20 @@ print_result(int *r){
 }
 
 int main(){
-    int *T,r;
+    int *T,*r;
     int size;
     N = 4;
 
-    int input[16] = {1,0,0,0, 0,1,1,1, 0,1,1,0, 1,0,1,1}; /*can use char input[16] to substitute int input[16],however,\
-                                                            the best method is a 16-bit number*/
+    int input[16] = {1,0,0,0, 0,1,1,1, 0,1,1,0, 1,0,1,1}; /*can use char input[16] to\
+                        substitute int input[16],however, the best method is a 16-bit number*/
     size = (N+1)*N*N;
-    if((T = malloc((size_t)size)) == NULL){
+    if((T = malloc((size_t)size*sizeof(int))) == NULL){
         printf("malloc failed\n");
         exit(1);
     }
-    memcpy(T,input,N*N*sizeof(int));
+    memcpy(T,input,sizeof(input));
     r = transitive_closure(T);
     print_result(r);
-    
     free(T);
     return 0;
 }
