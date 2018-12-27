ccdfa23921c02cc92ecb864f85f9b424e0778f5f
cilamoo <sgydd001@gmail.com>
2018-12-27 16:53:02 +0800

    fix bug in counting_sort.c

--- II Sorting and Order Statistics/8 Sorting in Linear Time/counting_sort.c
+++ II Sorting and Order Statistics/8 Sorting in Linear Time/counting_sort.c
@@ -17,20 +17,22 @@ void counting_sort(int *A, int *B, int n, int k){
     }
 
     for(j = n-1; j >= 0; j--){
-        B[C[A[j]]] = A[j];
+        B[C[A[j]]-1] = A[j];
         C[A[j]] -= 1;
     }
 
 }
 
 int main(){
+    int i;
     int B[8];
     int A[8] = {2,5,3,0,2,3,0,3};
     int k = 5;
     int n = 8;
     
     counting_sort(A,B,n,k);
-    print_result(B);
+    for(i = 0; i < n; i++)
+        printf("%d ", B[i]);
     
     return 0;
 }
